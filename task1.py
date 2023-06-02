75d4b62982c354c5c64ba03276206e2452c17636
Amy <amyy290@deltalearns.ca>
2023-06-02 11:33:57 -0700

    FINALLY WORKS!!!

--- task1.py
+++ task1.py
@@ -8,29 +8,54 @@ rectangle object through.
 """
 w = tk.Tk()
 w.geometry("925x475")
+w.title("Garden Maze")
 w.attributes('-topmost',True)
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
 
 f = open('map1.txt')
 map1 = f.readlines()
+
+mapimg = tk.PhotoImage(file="assets/map.forest.png")
+
 walls = []
-for i in range(1,15):
+
+for i in range (len(map1)):
     print(map1[i])
-    for i in range (len(map1)):
-        print(map1[i])
-        for j in range(len(map1[i])):
-            #print(map1[i][j])
-            if map1[i][j] == "*":
-                print(f"wall at {j},{i}")
-                c.create_rectangle(j,i,j,i)
+    for j in range(len(map1[i])):
+        if map1[i][j] == "*":
+            print(f"wall at {j},{i}")
+            img = c.create_image(j*30+30,i*30+30,image=mapimg)
+            walls.append(img)
+
+rec = c.create_rectangle(50,50,70,70,fill="SkyBlue")
 
 
-'''
+def keyPress(e):
+    print(e)
+    print(e.keycode, e.keysym, e.x, e.y)
+    r = c.coords(rec)
 
-for i in range(1,15):
-    print(map1[i])
-'''
+    if e.keysym == 'Left':
+        if r[0] > 50:
+            c.move(rec,-5,0)
+    elif e.keysym == 'Right':
+        if r[2] < 850:
+            c.move(rec,5,0)
+    elif e.keysym == 'Up':
+        if r[1] > 50:
+            c.move(rec,0,-5)
+    elif e.keysym == 'Down':
+        if r[3] < 430:
+            c.move(rec,0,5)
+
+
+w.bind("<Left>",keyPress)
+w.bind("<Right>",keyPress)
+w.bind("<Up>",keyPress)
+w.bind("<Down>",keyPress)
+
+
 f.close()
 
 w.mainloop()
--- task2.py
+++ task2.py
@@ -9,7 +9,7 @@ Legend:
 1 Plains
 2 Forest
 3 Hills
-4 Mountains
+4 Mountain
 5 Swamp
 6 City
 """
@@ -21,6 +21,41 @@ c.pack()
 
 f = open('map2.txt')
 map2 = f.readlines()
-print(map2[0])
+
+water = tk.PhotoImage(file="assets/map.water.png")
+plains = tk.PhotoImage(file="assets/map.plains.png")
+forest = tk.PhotoImage(file="assets/map.forest.png")
+hills = tk.PhotoImage(file="assets/map.hills.png")
+mountain = tk.PhotoImage(file="assets/map.mountain.png")
+swamp = tk.PhotoImage(file="assets/map.swamp.png")
+city = tk.PhotoImage(file="assets/map.city.png")
+walls = []
+
+for i in range(len(map2)):
+    for j in range (len(map2[i])):
+        print(f"walls at {j},{i}")
+        if map2[i][j] == "0":
+            img = c.create_image(j*30+30,i*30+30,image=water)
+            walls.append(img)
+        elif map2[i][j] == "1":
+            img = c.create_image(j*30+30,i*30+30,image=plains)
+            walls.append(img)
+        elif map2[i][j] == "2":
+            img = c.create_image(j*30+30,i*30+30,image=forest)
+            walls.append(img)
+        elif map2[i][j] == "3":
+            img = c.create_image(j*30+30,i*30+30,image=hills)
+            walls.append(img)
+        elif map2[i][j] == "4":
+            img = c.create_image(j*30+30,i*30+30,image=mountain)
+            walls.append(img)
+        elif map2[i][j] == "5":
+            img = c.create_image(j*30+30,i*30+30,image=swamp)
+            walls.append(img)
+        elif map2[i][j] == "6":
+            img = c.create_image(j*30+30,i*30+30,image=city)
+            walls.append(img)
+        
+
 
 w.mainloop()
